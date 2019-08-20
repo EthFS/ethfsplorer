c7a861c55d551a53cbf2c687e7368eba081c681b
Hector Chu <hectorchu@gmail.com>
2019-08-20 11:06:35 +0100

    Error handling

--- src/components/modals/NewFolder.js
+++ src/components/modals/NewFolder.js
@@ -1,19 +1,25 @@
 import * as Path from 'path'
 import React, {useState} from 'react'
-import {Form, FormGroup, Label, Input} from 'reactstrap'
+import {Form, FormGroup, FormFeedback, Label, Input} from 'reactstrap'
 import Modal from './Modal'
 import {useKernel} from '../../web3/kernel'
 import {utf8ToHex} from 'web3-utils'
+import errno from 'errno'
 
 export default function NewFolder({address, path, isOpen, toggle}) {
   const [name, setName] = useState('')
+  const [error, setError] = useState('')
   const kernel = useKernel(address)
   async function handleOk(e) {
     e.preventDefault()
     if (name === '') return
-    await kernel.mkdir(utf8ToHex(Path.join(path, name)))
-    setName('')
-    toggle()
+    try {
+      await kernel.mkdir(utf8ToHex(Path.join(path, name)))
+      setName('')
+      toggle()
+    } catch (e) {
+      setError(errno.code[e.reason].description)
+    }
   }
   return (
     <Modal isOpen={isOpen} title="New Folder" toggle={toggle} onOk={handleOk} allowOk={name !== ''}>
@@ -23,9 +29,14 @@ export default function NewFolder({address, path, isOpen, toggle}) {
             type="text"
             value={name}
             placeholder="Enter name"
-            onChange={e => setName(e.target.value)}
+            onChange={e => {
+              setName(e.target.value)
+              setError('')
+            }}
             spellCheck="false"
+            invalid={!!error}
           />
+          <FormFeedback>{error}</FormFeedback>
         </FormGroup>
       </Form>
     </Modal>
