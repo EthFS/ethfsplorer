0f34ea3adfaef5408206ef4c9a8ff218f000551b
Hector Chu <hectorchu@gmail.com>
2019-08-22 12:32:45 +0100

    Select basename in rename

--- src/components/modals/Rename.js
+++ src/components/modals/Rename.js
@@ -1,5 +1,5 @@
 import * as Path from 'path'
-import React, {useState, useEffect} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {Form, FormGroup, FormFeedback, Input} from 'reactstrap'
 import Modal from './Modal'
 import {useKernel} from '../../web3/kernel'
@@ -10,8 +10,22 @@ import {emit} from '../../utils/events'
 export default function Rename({address, path, isOpen, toggle}) {
   const [newPath, setNewPath] = useState('')
   const [error, setError] = useState('')
+  const [fresh, setFresh] = useState()
   const kernel = useKernel(address)
-  useEffect(() => setNewPath(path), [isOpen])
+  const input = useRef()
+  useEffect(() => {
+    if (!isOpen) return
+    setNewPath(path)
+    setError('')
+    setFresh(true)
+  }, [isOpen])
+  useEffect(() => {
+    if (!fresh) return
+    const {length} = Path.basename(path)
+    input.current.setSelectionRange(path.length-length, path.length)
+    input.current.focus()
+    setFresh()
+  })
   async function handleOk(e) {
     e.preventDefault()
     if (newPath === '' || newPath === path) return
@@ -35,6 +49,7 @@ export default function Rename({address, path, isOpen, toggle}) {
       <Form onSubmit={handleOk}>
         <FormGroup>
           <Input
+            innerRef={input}
             type="text"
             value={newPath}
             placeholder="Enter new name"
