7430d5ce11c321e44369ec571c8c7d6bf4dc1fc4
Hector Chu <hectorchu@gmail.com>
2019-08-23 22:28:39 +0100

    Better rename UI

--- src/components/modals/Rename.js
+++ src/components/modals/Rename.js
@@ -1,6 +1,6 @@
 import * as Path from 'path'
 import React, {useState, useEffect, useRef} from 'react'
-import {Form, FormGroup, FormFeedback, Input} from 'reactstrap'
+import {Form, FormGroup, Input, Progress} from 'reactstrap'
 import Modal from './Modal'
 import {useKernel} from '../../web3/kernel'
 import {utf8ToHex} from 'web3-utils'
@@ -10,13 +10,15 @@ import useTrigger from '../../utils/trigger'
 
 export default function Rename({address, path, isOpen, toggle}) {
   const [newPath, setNewPath] = useState('')
-  const [error, setError] = useState('')
+  const [progress, setProgress] = useState()
+  const [progressText, setProgressText] = useState('')
+  const [error, setError] = useState()
   const kernel = useKernel(address)
   const input = useRef()
   useEffect(() => {
     if (!isOpen) return
     setNewPath(path)
-    setError('')
+    setProgress()
   }, [isOpen])
   useTrigger(isOpen, () => {
     const {length} = Path.basename(path)
@@ -35,12 +37,16 @@ export default function Rename({address, path, isOpen, toggle}) {
     const path2 = pathToAbsolute(newPath)
     if (path2 === path) return
     try {
+      setError()
+      setProgress(100)
+      setProgressText(`Renaming ${path} to ${path2}`)
       await kernel.move(utf8ToHex(path), utf8ToHex(path2))
       toggle()
       emit('refresh-path', Path.dirname(path))
     } catch (e) {
       const err = errno.code[e.reason]
       setError(err ? err.description : e.message)
+      setTimeout(setProgress, 3e3)
     }
   }
   return (
@@ -49,8 +55,11 @@ export default function Rename({address, path, isOpen, toggle}) {
       title="Rename"
       toggle={toggle}
       onOk={handleOk}
-      allowOk={newPath !== '' && pathToAbsolute(newPath) !== path}
-      >
+      allowOk={
+        newPath !== '' &&
+        pathToAbsolute(newPath) !== path &&
+        progress === undefined
+      }>
       <Form onSubmit={handleOk}>
         <FormGroup>
           <Input
@@ -63,11 +72,14 @@ export default function Rename({address, path, isOpen, toggle}) {
               setError('')
             }}
             spellCheck="false"
-            invalid={!!error}
           />
-          <FormFeedback>{error}</FormFeedback>
         </FormGroup>
       </Form>
+      {progress >= 0 && (error ?
+        <Progress animated color="danger" value={progress}>{error}</Progress>
+        :
+        <Progress animated value={progress}>{progressText}</Progress>
+      )}
     </Modal>
   )
 }
